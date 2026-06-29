f9cd9fa416e24754eadcc5cbc13627192c2dfbcc
shubhkha10 <shubh2425m@gmail.com>
2026-06-29 10:02:07 -0700

    Debug database connection

--- shalizasoft-backend/config/db.js
+++ shalizasoft-backend/config/db.js
@@ -1,18 +1,18 @@
-console.log("HOST:", process.env.MYSQLHOST);
-console.log("USER:", process.env.MYSQLUSER);
-console.log("DB:", process.env.MYSQLDATABASE);
-console.log("PORT:", process.env.MYSQLPORT);
 const mysql = require("mysql2");
+
+console.log("HOST:", process.env.MYSQLHOST);
+console.log("DATABASE:", process.env.MYSQLDATABASE);
+console.log("USER:", process.env.MYSQLUSER);
+console.log("PORT:", process.env.MYSQLPORT);
 
 const db = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  port: Number(process.env.MYSQLPORT),
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
 });
 
 module.exports = db;
