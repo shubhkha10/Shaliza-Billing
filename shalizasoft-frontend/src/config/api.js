f9d46fabb828b7bb888c6a349c48df7d07df33bf
shubhkha10 <shubh2425m@gmail.com>
2026-06-30 12:40:50 -0700

    Fix deployment

--- shalizasoft-frontend/src/config/api.js
+++ shalizasoft-frontend/src/config/api.js
@@ -1,10 +1,3 @@
-import axios from "axios";
-
-export const BASE_URL =
-  "https://shaliza-billing-production.up.railway.app/api";
-
-const API = axios.create({
-  baseURL: BASE_URL,
-});
+const API = "https://shaliza-billing-production.up.railway.app/api";
 
 export default API;
