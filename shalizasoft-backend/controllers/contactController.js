ff3d597c9aaa3ce92b11dc8f07fd93045b048554
shubhkha10 <shubh2425m@gmail.com>
2026-07-01 03:15:16 -0700

    fix razorpay order crash

--- shalizasoft-backend/controllers/contactController.js
+++ shalizasoft-backend/controllers/contactController.js
@@ -1,4 +1,4 @@
-const db = require("../config/db");
+// const db = require("../config/db");
 
 exports.sendContactMessage = (req, res) => {
   const userId = req.user?.id || null;
--- shalizasoft-frontend/src/pages/Subscription.js
+++ shalizasoft-frontend/src/pages/Subscription.js
@@ -230,7 +230,7 @@
 // export default Subscription;
 const Razorpay = require("razorpay");
 const crypto = require("crypto");
-const db = require("../config/db");
+// const db = require("../config/db");
 
 // ================================
 // SAFE RAZORPAY INIT
